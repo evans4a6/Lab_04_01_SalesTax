0c1f2a86f3998c2018483d4fe66711a9bf7af40c
addisonevans <evans4a6@mail.uc.edu>
2026-02-08 20:00:53 -0500

    Lab 4 Task 1 complete

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    final double TAX_RATE = 0.05;
-    int price = 18;
-    double finalSale = 0.0;
-    double salesTax = 0.0;
+        final double TAX_RATE = 0.05;
+        int price = 18;
+        double finalSale = 0.0;
+        double salesTax = 0.0;
 
-    salesTax = price * TAX_RATE;
-    finalSale = salesTax + price;
-    System.out.println("The price of your sale was " + price + " dollars, and the tax for this sale is " + salesTax + " dollars. Your final total is " + finalSale + " dollars");
-
-
+        salesTax = price * TAX_RATE;
+        finalSale = salesTax + price;
+        System.out.println("The price of your sale was " + price + " dollars, and the tax for this sale is " + salesTax + " dollars. Your final total is " + finalSale + " dollars");
 
     }
 }
